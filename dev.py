21f92eb674c4b20ebc2fcd75e7cd43613dcd01d7
João Palmeiro <joaommpalmeiro@gmail.com>
2022-11-26 00:10:33 +0000

    Update dev.py

--- dev.py
+++ dev.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
-from pandas_sphinx import to_sphinx
+from pandas_sphinx import __version__, to_sphinx
 
 if __name__ == "__main__":
+    print(__version__)
+
     # Source: https://github.com/gregbanks/python-tabulate#readme
     df = pd.DataFrame(
         [["spam", 42], ["eggs", 451], ["bacon", 0]], columns=["item", "qty"]
